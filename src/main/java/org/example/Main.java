a2ae104025cd8dc42db0c56bb77b3526a57aa674
JDW-ehb <133092166+JDW-ehb@users.noreply.github.com>
2023-11-29 17:33:01 +0100

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        System.out.println(Core.NATIVE_LIBRARY_NAME);
+
+        //System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        //Mat image = Imgcodecs.imread("src/Images/WIN_20231129_17_02_39_Pro.jpg");
+
+
+
+    }
+
+
+
+}
